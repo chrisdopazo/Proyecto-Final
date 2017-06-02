dcf44004be8d52804fd10616e606b6ba3f1856e2
fg dgd <fg dgd>
2017-06-02 00:16:35 -0300

    php files

---
+++ siosi/registracion.php
@@ -0,0 +1,49 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+include 'conn.php';
+
+ $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+
+ $nombre = $_POST['nombre'];
+ $apellido = $_POST['apellido'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+
+ 
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo  "Ingrese un Email Correcto"; 
+  die;
+}else if(strlen(trim($password)) < 8){
+	echo "La contraseña tiene que tener mas de 8 caracteres";	
+	
+}
+else{
+
+ $CheckSQL = "SELECT * FROM userinfo WHERE email='$email'";
+ 
+ $check = mysqli_fetch_array(mysqli_query($con,$CheckSQL));
+ 
+ if(isset($check)){
+
+ echo 'Ya hay una cuenta registrada con este Email';
+
+ }
+else{ 
+$Sql_Query = "INSERT INTO userinfo (nombre,apellido,email,password) values ('$nombre','$apellido','$email','$password')";
+
+ if(mysqli_query($con,$Sql_Query))
+{
+ echo 'Cuenta Creada . Muchas Gracias!';
+}
+else
+{
+ echo 'Hubo un Error . Intente de Nuevo';
+ }
+ }
+}
+}
+
+ mysqli_close($con);
+?>
